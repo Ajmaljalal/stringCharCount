4f742b2a194e4e19b3fee6eae644c7a599c2d164
Ajmal Jalal <ajmaljalal58@gmail.com>
2016-01-23 11:07:46 -0800

    code format change for a better solution

--- stringCharCount.js
+++ stringCharCount.js
@@ -2,9 +2,10 @@ function count(string) {
     if (!string) return {}
     const result = {}
     string.split('').forEach(char => {
-        if (result[char]) {
-            result[char] += 1
-        } else result[char] = 1
+        // if (result[char]) {
+        //     result[char] += 1
+        // } else result[char] = 1
+        result[char] ? result[char] += 1 : result[char] = 1
     })
     return result
 }
